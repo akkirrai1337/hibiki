61b07a9fefa69e1d3716ddcb10c6d0e77e111666
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-27 15:07:13 +0300

    Share library entries layout

---
+++ shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/library/LibraryLayoutMetrics.kt
@@ -0,0 +1,9 @@
+package org.akkirrai.hibiki.shared.library
+
+import androidx.compose.ui.unit.dp
+
+val LibraryEntriesRowGap = 14.dp
+val LibraryEntriesRefreshIndicatorSize = 20.dp
+val LibraryEntriesRefreshIndicatorStrokeWidth = 2.dp
+val LibraryEntriesHorizontalPadding = 16.dp
+val LibraryEntriesItemGap = 12.dp
