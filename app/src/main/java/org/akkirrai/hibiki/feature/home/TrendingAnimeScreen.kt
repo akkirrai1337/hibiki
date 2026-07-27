2bf7764e8dd38e845b48a2545471d871af170447
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-27 17:37:09 +0300

    Clean trending imports

--- app/src/main/java/org/akkirrai/hibiki/feature/home/TrendingAnimeScreen.kt
+++ app/src/main/java/org/akkirrai/hibiki/feature/home/TrendingAnimeScreen.kt
@@ -12,7 +12,6 @@ import androidx.compose.material.icons.outlined.Image
 import androidx.compose.material.icons.outlined.WarningAmber
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
