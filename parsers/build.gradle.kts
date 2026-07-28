dbbd0e6a3c6eff0d1aa5c5195fb33b67e181b81c
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-29 02:29:39 +0300

    Move mirror execution to commonMain

--- parsers/build.gradle.kts
+++ parsers/build.gradle.kts
@@ -17,6 +17,7 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
                 implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
             }
         }
