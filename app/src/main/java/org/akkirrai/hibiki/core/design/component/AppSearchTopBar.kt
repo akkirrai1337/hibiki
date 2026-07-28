e073952cedbf801a8619addc563ae549cc58cfc6
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-28 12:47:44 +0300

    Clean search bar adapter

--- app/src/main/java/org/akkirrai/hibiki/core/design/component/AppSearchTopBar.kt
+++ app/src/main/java/org/akkirrai/hibiki/core/design/component/AppSearchTopBar.kt
@@ -1,32 +1,13 @@
 package org.akkirrai.hibiki.core.design.component
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material.icons.outlined.FilterList
 import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import org.akkirrai.hibiki.R
-import org.akkirrai.hibiki.shared.design.UiDimens
 
 @Composable
 fun AppSearchTopBar(
