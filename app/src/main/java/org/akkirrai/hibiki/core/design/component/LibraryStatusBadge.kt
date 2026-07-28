0795d442369f88d43bdc7a2b84c8cea0b9208f1b
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-28 12:45:56 +0300

    Clean library badge imports

--- app/src/main/java/org/akkirrai/hibiki/core/design/component/LibraryStatusBadge.kt
+++ app/src/main/java/org/akkirrai/hibiki/core/design/component/LibraryStatusBadge.kt
@@ -1,24 +1,13 @@
 package org.akkirrai.hibiki.core.design.component
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.unit.sp
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.lifecycle.compose.LocalLifecycleOwner
