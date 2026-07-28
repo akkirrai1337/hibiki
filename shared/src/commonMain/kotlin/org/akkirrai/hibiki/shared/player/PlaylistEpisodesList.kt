828b983a3401f555ec134294d82602e083fd963b
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-28 13:09:25 +0300

    Clean playlist list imports

--- shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/player/PlaylistEpisodesList.kt
+++ shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/player/PlaylistEpisodesList.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.Dp
-import androidx.compose.ui.unit.dp
 import org.akkirrai.hibiki.shared.model.WatchEpisode
 
 @Composable
