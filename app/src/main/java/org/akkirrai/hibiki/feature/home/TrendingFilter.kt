0b433c0dc43b6b85b1c64bf5689856ca864dfe57
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-28 22:44:51 +0300

    Share trending filter localization keys

--- app/src/main/java/org/akkirrai/hibiki/feature/home/TrendingFilter.kt
+++ app/src/main/java/org/akkirrai/hibiki/feature/home/TrendingFilter.kt
@@ -2,11 +2,13 @@ package org.akkirrai.hibiki.feature.home
 
 import org.akkirrai.hibiki.R
 import org.akkirrai.hibiki.shared.home.TrendingFilter
+import org.akkirrai.hibiki.shared.home.localizationKey
 
 @get:androidx.annotation.StringRes
 val TrendingFilter.titleResId: Int
-    get() = when (this) {
-        TrendingFilter.All -> R.string.trending_filter_all
-        TrendingFilter.Movies -> R.string.trending_filter_movies
-        TrendingFilter.Ona -> R.string.trending_filter_ona
+    get() = when (localizationKey()) {
+        "trending_filter_all" -> R.string.trending_filter_all
+        "trending_filter_movies" -> R.string.trending_filter_movies
+        "trending_filter_ona" -> R.string.trending_filter_ona
+        else -> error("Unknown trending filter localization key")
     }
