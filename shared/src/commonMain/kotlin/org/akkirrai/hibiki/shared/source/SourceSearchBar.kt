c9975eaa21af0a166eb24ae9a1517815fe24e1fd
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-28 13:13:33 +0300

    Clean source search imports

--- shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/source/SourceSearchBar.kt
+++ shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/source/SourceSearchBar.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.unit.dp
 import org.akkirrai.hibiki.shared.design.UiDimens
 import org.akkirrai.hibiki.shared.design.component.AppSearchTopBar
 
--- shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/source/SourceSearchSection.kt
+++ shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/source/SourceSearchSection.kt
@@ -20,7 +20,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 import org.akkirrai.hibiki.shared.design.UiDimens
 
 @Composable
