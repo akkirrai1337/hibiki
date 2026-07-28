714ea137c420c42b1fd22f3e4a55c8e1df13ce90
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-28 16:56:58 +0300

    Add shared player playlist button default

--- shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/player/PlayerPlaylistButton.kt
+++ shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/player/PlayerPlaylistButton.kt
@@ -1,7 +1,11 @@
 package org.akkirrai.hibiki.shared.player
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.PlaylistPlay
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import org.akkirrai.hibiki.shared.design.component.AppFilledIconButton
 import org.akkirrai.hibiki.shared.design.component.AppFilledIconButtonStyle
 
@@ -16,5 +20,24 @@ fun AppPlayerPlaylistButton(
         modifier = modifier,
         style = AppFilledIconButtonStyle.DarkOverlay,
         content = iconContent,
+    )
+}
+
+@Composable
+fun AppPlayerPlaylistButton(
+    onClick: () -> Unit,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+) {
+    AppPlayerPlaylistButton(
+        onClick = onClick,
+        modifier = modifier,
+        iconContent = {
+            Icon(
+                imageVector = Icons.AutoMirrored.Outlined.PlaylistPlay,
+                contentDescription = contentDescription,
+                tint = Color.White,
+            )
+        },
     )
 }
