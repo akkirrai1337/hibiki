3d600093cdafc5b587ebef222e7101a05afaa184
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-28 13:02:57 +0300

    Clean player feedback imports

--- shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/player/PlayerSkipSegmentOverlay.kt
+++ shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/player/PlayerSkipSegmentOverlay.kt
@@ -9,7 +9,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun AppPlayerSkipSegmentOverlay(
