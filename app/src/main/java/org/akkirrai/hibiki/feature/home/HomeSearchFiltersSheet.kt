c4bda683ed4040555df742d6db039192646571f0
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-28 12:41:48 +0300

    Clean filter sheet imports

--- app/src/main/java/org/akkirrai/hibiki/feature/home/HomeSearchFiltersSheet.kt
+++ app/src/main/java/org/akkirrai/hibiki/feature/home/HomeSearchFiltersSheet.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
