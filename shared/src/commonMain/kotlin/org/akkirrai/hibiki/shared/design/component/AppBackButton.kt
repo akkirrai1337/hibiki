46374215349962681b13cb32b74c893039f00e85
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-28 16:48:37 +0300

    Add shared back button default

--- shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/design/component/AppBackButton.kt
+++ shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/design/component/AppBackButton.kt
@@ -1,8 +1,15 @@
 package org.akkirrai.hibiki.shared.design.component
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.CompositingStrategy
+import androidx.compose.ui.graphics.graphicsLayer
 
 @Composable
 fun AppBackButton(
@@ -14,4 +21,29 @@ fun AppBackButton(
     IconButton(onClick = onClick, enabled = enabled, modifier = modifier) {
         iconContent()
     }
+}
+
+@Composable
+fun AppBackButton(
+    onClick: () -> Unit,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+) {
+    AppBackButton(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = enabled,
+        iconContent = {
+            Icon(
+                imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
+                contentDescription = contentDescription,
+                tint = Color.White,
+                modifier = Modifier.graphicsLayer {
+                    compositingStrategy = CompositingStrategy.Offscreen
+                    blendMode = BlendMode.Difference
+                },
+            )
+        },
+    )
 }
