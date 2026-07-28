d6f7d5100944d52581681bbe3ab7bd81384533f5
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-28 23:45:10 +0300

    Reuse shared library language resolution

--- shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/library/LibraryLanguageResolver.kt
+++ shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/library/LibraryLanguageResolver.kt
@@ -1,12 +1,9 @@
 package org.akkirrai.hibiki.shared.library
 
 import org.akkirrai.hibiki.shared.settings.LanguageMode
+import org.akkirrai.hibiki.shared.settings.resolveAppLanguageTag
 
 fun isRussianLibraryLanguage(
     languageMode: LanguageMode,
     systemLanguage: String,
-): Boolean = when (languageMode) {
-    LanguageMode.RUSSIAN -> true
-    LanguageMode.ENGLISH -> false
-    LanguageMode.SYSTEM -> systemLanguage.lowercase() == "ru"
-}
+): Boolean = resolveAppLanguageTag(languageMode, systemLanguage) == "ru"
