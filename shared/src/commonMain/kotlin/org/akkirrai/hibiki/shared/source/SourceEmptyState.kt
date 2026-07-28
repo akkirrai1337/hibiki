464888a09bb3fd0c0a21eb73fa5fb865024c7ab6
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-28 13:10:54 +0300

    Clean source component imports

--- shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/source/SourceEmptyState.kt
+++ shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/source/SourceEmptyState.kt
@@ -8,7 +8,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun SourceEmptyState(
--- shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/source/SourceItemCard.kt
+++ shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/source/SourceItemCard.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
 
 @Composable
 fun SourceItemCard(
