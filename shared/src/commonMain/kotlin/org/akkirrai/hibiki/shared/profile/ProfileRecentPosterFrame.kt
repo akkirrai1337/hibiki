ec28d53305d62017a78de73391328f44a55a9fc4
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-28 12:55:01 +0300

    Clean profile poster imports

--- shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/profile/ProfileRecentPosterFrame.kt
+++ shared/src/commonMain/kotlin/org/akkirrai/hibiki/shared/profile/ProfileRecentPosterFrame.kt
@@ -2,7 +2,6 @@ package org.akkirrai.hibiki.shared.profile
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
