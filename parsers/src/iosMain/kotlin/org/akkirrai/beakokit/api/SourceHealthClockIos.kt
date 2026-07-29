ba1464edc5cce0e1abc8c2f2b1d4d15a282ec34f
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-30 00:05:56 +0300

    Align iOS source health actual

--- parsers/src/iosMain/kotlin/org/akkirrai/beakokit/api/SourceHealthClockIos.kt
+++ parsers/src/iosMain/kotlin/org/akkirrai/beakokit/api/SourceHealthClockIos.kt
@@ -2,5 +2,6 @@ package org.akkirrai.beakokit.api
 
 import platform.Foundation.NSProcessInfo
 
+@PublishedApi
 internal actual fun monotonicTimeMillis(): Long =
     (NSProcessInfo.processInfo.systemUptime * 1_000).toLong()
