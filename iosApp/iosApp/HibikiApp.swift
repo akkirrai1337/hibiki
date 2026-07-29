d2e2953b1dada11948507c127798fec1e7c80e76
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-30 00:15:51 +0300

    Use UIKit as iOS app host

--- iosApp/iosApp/HibikiApp.swift
+++ iosApp/iosApp/HibikiApp.swift
@@ -1,10 +1,20 @@
-import SwiftUI
+import UIKit
+import shared
 
 @main
-struct HibikiApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+final class HibikiAppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = MainViewControllerKt.MainViewController(
+            systemLanguage: Locale.current.languageCode ?? "en"
+        )
+        window.makeKeyAndVisible()
+        self.window = window
+        return true
     }
 }
