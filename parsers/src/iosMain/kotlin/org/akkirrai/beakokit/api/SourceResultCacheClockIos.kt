d4044fbd184101932c403587b457e3da8b1b9b18
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-29 13:39:51 +0300

    Fix iOS Foundation clock imports

--- parsers/src/iosMain/kotlin/org/akkirrai/beakokit/api/SourceResultCacheClockIos.kt
+++ parsers/src/iosMain/kotlin/org/akkirrai/beakokit/api/SourceResultCacheClockIos.kt
@@ -1,6 +1,7 @@
 package org.akkirrai.beakokit.api
 
 import platform.Foundation.NSDate
+import platform.Foundation.timeIntervalSince1970
 
 internal actual fun currentWallClockMillis(): Long =
     (NSDate().timeIntervalSince1970 * 1_000).toLong()
--- parsers/src/iosMain/kotlin/org/akkirrai/beakokit/source/aniliberty/internal/AniLibertyClockIos.kt
+++ parsers/src/iosMain/kotlin/org/akkirrai/beakokit/source/aniliberty/internal/AniLibertyClockIos.kt
@@ -1,6 +1,7 @@
 package org.akkirrai.beakokit.source.aniliberty.internal
 
 import platform.Foundation.NSDate
+import platform.Foundation.timeIntervalSince1970
 
 internal actual fun currentTimeMillis(): Long =
     (NSDate().timeIntervalSince1970 * 1_000).toLong()
