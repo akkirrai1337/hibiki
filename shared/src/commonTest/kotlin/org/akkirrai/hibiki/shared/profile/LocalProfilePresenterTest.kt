5cde556f9d84c8c1dbe13f1ddabde1e794363309
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-07-31 16:44:39 +0300

    Test shared profile reload lifecycle

--- shared/src/commonTest/kotlin/org/akkirrai/hibiki/shared/profile/LocalProfilePresenterTest.kt
+++ shared/src/commonTest/kotlin/org/akkirrai/hibiki/shared/profile/LocalProfilePresenterTest.kt
@@ -1,32 +1,34 @@
 package org.akkirrai.hibiki.shared.profile
 
+import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
-import kotlinx.coroutines.test.runTest
 
 class LocalProfilePresenterTest {
     @Test
-    fun updatesProfileFields() {
+    fun loadPublishesSharedDataAndClearsLoadingState() = runTest {
         val presenter = LocalProfilePresenter()
+        val expected = LocalProfileData(profileName = "Vadim")
 
-        presenter.updateProfileName("Demo")
-        presenter.updateProfileAvatar("content://avatar")
+        presenter.load(object : LocalProfileDataRepository {
+            override suspend fun load(): LocalProfileData = expected
+        })
 
-        assertEquals("Demo", presenter.state.value.data.profileName)
-        assertEquals("content://avatar", presenter.state.value.data.profileAvatarUri)
+        assertEquals(expected, presenter.state.value.data)
+        assertFalse(presenter.state.value.isLoading)
     }
 
     @Test
-    fun loadsDataThroughRepository() = runTest {
+    fun failedLoadDoesNotLeavePresenterLoading() = runTest {
         val presenter = LocalProfilePresenter()
-        val repository = object : LocalProfileDataRepository {
-            override suspend fun load(): LocalProfileData = LocalProfileData(profileName = "Loaded")
+
+        runCatching {
+            presenter.load(object : LocalProfileDataRepository {
+                override suspend fun load(): LocalProfileData = error("load failed")
+            })
         }
 
-        presenter.load(repository)
-
-        assertEquals("Loaded", presenter.state.value.data.profileName)
         assertFalse(presenter.state.value.isLoading)
     }
 }
