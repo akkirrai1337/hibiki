65c08196a3a679f240cb15bcb6116ec1c611d29b
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-08-01 14:24:37 +0300

    Use Compose UI test v2 API

--- app-desktop/src/test/kotlin/org/akkirrai/hibiki/desktop/DesktopComposeTestHarnessTest.kt
+++ app-desktop/src/test/kotlin/org/akkirrai/hibiki/desktop/DesktopComposeTestHarnessTest.kt
@@ -6,7 +6,7 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.runComposeUiTest
+import androidx.compose.ui.test.v2.runComposeUiTest
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
--- app-desktop/src/test/kotlin/org/akkirrai/hibiki/desktop/SharedWatchSourcesComposeTest.kt
+++ app-desktop/src/test/kotlin/org/akkirrai/hibiki/desktop/SharedWatchSourcesComposeTest.kt
@@ -1,11 +1,11 @@
 package org.akkirrai.hibiki.desktop
 
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
-import androidx.compose.ui.test.runComposeUiTest
+import androidx.compose.ui.test.v2.runComposeUiTest
 import org.akkirrai.hibiki.shared.model.WatchSource
 import org.akkirrai.hibiki.shared.player.AppWatchSourcesContent
 import org.akkirrai.hibiki.shared.player.WatchSourcesScreenState
