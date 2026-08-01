e3c86532952a06b1f214b266bd9f9379c8ecc80c
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-08-01 14:29:55 +0300

    Test shared details back navigation

--- app-desktop/src/test/kotlin/org/akkirrai/hibiki/desktop/SharedDetailsComposeTest.kt
+++ app-desktop/src/test/kotlin/org/akkirrai/hibiki/desktop/SharedDetailsComposeTest.kt
@@ -3,6 +3,7 @@ package org.akkirrai.hibiki.desktop
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.test.ExperimentalTestApi
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.compose.ui.test.v2.runComposeUiTest
@@ -46,5 +47,32 @@ class SharedDetailsComposeTest {
             .performClick()
 
         assertEquals(1, watchClicks)
+    }
+
+    @Test
+    fun detailsScreenBackButtonDeliversCommonBackAction() = runComposeUiTest {
+        var backClicks = 0
+
+        setContent {
+            MaterialTheme {
+                AppDetailsScreen(
+                    anime = Anime(
+                        id = "ani-liberty:test-title",
+                        title = "Test anime",
+                        subtitle = "TV | 2026",
+                        episodesLabel = "12 episodes",
+                        status = "Ongoing",
+                    ),
+                    onBackClick = { backClicks++ },
+                    onRelatedAnimeClick = {},
+                )
+            }
+        }
+
+        onNodeWithContentDescription("Back")
+            .assertIsDisplayed()
+            .performClick()
+
+        assertEquals(1, backClicks)
     }
 }
