8cae20f2fc01e513eaeaf139291ed0ec1f3a9aed
akkirrai1337 <193419044+akkirrai1337@users.noreply.github.com>
2026-08-02 21:25:03 +0300

    Add external source Wasm fixture

--- tools/wasmtime-spike/src/lib.rs
+++ tools/wasmtime-spike/src/lib.rs
@@ -155,31 +155,7 @@ fn run_protocol_host_call_request(
     let engine = Engine::default();
     let module = Module::new(
         &engine,
-        wat::parse_str(
-            r#"
-                (module
-                    (import "host" "call" (func $host_call (param i32 i32) (result i64)))
-                    (memory (export "memory") 2)
-                    (global $heap (mut i32) (i32.const 4096))
-                    (func (export "beakokit_reset")
-                        i32.const 4096
-                        global.set $heap
-                    )
-                    (func (export "beakokit_alloc") (param i32) (result i32)
-                        global.get $heap
-                        global.get $heap
-                        local.get 0
-                        i32.add
-                        global.set $heap
-                    )
-                    (func (export "beakokit_call") (param i32 i32) (result i64)
-                        local.get 0
-                        local.get 1
-                        call $host_call
-                    )
-                )
-            "#,
-        )?,
+        wat::parse_str(include_str!("../fixtures/minimal-source.wat"))?,
     )?;
 
     let mut linker = Linker::new(&engine);
